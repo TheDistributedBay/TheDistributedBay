6d54882f0d7cdacc690ea19602c196add7acb3e4
Colin L. Rice <colin@daedrum.net>
2014-12-21 22:38:32 -0800

    Add listener interface, clean up handler code

--- database/database.go
+++ database/database.go
@@ -2,33 +2,58 @@ package database
 
 import (
 	"errors"
+	"sync"
 )
 
 type TorrentDB struct {
 	torrents map[string]*Torrent
+	writers  []TorrentWriter
+	lock     *sync.RWMutex
 }
 
 func NewTorrentDB() *TorrentDB {
 	t := make(map[string]*Torrent)
-	return &TorrentDB{t}
+	return &TorrentDB{t, nil, &sync.RWMutex{}}
 }
 
-func (t *TorrentDB) Get(hash string) (*Torrent, error) {
-	torrent, ok := t.torrents[hash]
+func (db *TorrentDB) Get(hash string) (*Torrent, error) {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+	torrent, ok := db.torrents[hash]
 	if !ok {
 		return nil, errors.New("No such hash stored")
 	}
 	return torrent, nil
 }
 
-func (t *TorrentDB) Add(r *Torrent) {
-	t.torrents[r.Hash] = r
+func (db *TorrentDB) Add(t *Torrent) {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+	_, ok := db.torrents[t.Hash]
+	if ok {
+		return
+	}
+	db.torrents[t.Hash] = t
+	for _, w := range db.writers {
+		go w.NewTorrent(t)
+	}
 }
 
-func (t *TorrentDB) List() []string {
-	ts := make([]string, 0, len(t.torrents))
-	for _, r := range t.torrents {
+func (db *TorrentDB) List() []string {
+	db.lock.RLock()
+	defer db.lock.RUnlock()
+	ts := make([]string, 0, len(db.torrents))
+	for _, r := range db.torrents {
 		ts = append(ts, r.Hash)
 	}
 	return ts
+}
+
+func (db *TorrentDB) AddClient(w TorrentWriter) {
+	db.lock.Lock()
+	defer db.lock.Unlock()
+	db.writers = append(db.writers, w)
+	for _, t := range db.torrents {
+		go w.NewTorrent(t)
+	}
 }
--- network/connection_handler.go
+++ network/connection_handler.go
@@ -2,28 +2,40 @@ package network
 
 import (
 	"log"
+	"sync"
 
 	"github.com/TheDistributedBay/TheDistributedBay/database"
 )
 
 type ConnectionHandler struct {
-	t  *Transcoder
-	db database.Database
+	t           *Transcoder
+	db          database.Database
+	torrentList map[string]struct{}
+	lock        *sync.RWMutex
 }
 
 func NewConnectionHandler(t *Transcoder, db database.Database) *ConnectionHandler {
-	h := &ConnectionHandler{t, db}
-	go h.shovel()
-	return h
-}
+	h := &ConnectionHandler{t, db, nil, &sync.RWMutex{}}
 
-// Shovels torrents into and out of the db
-func (h *ConnectionHandler) shovel() {
 	m := make(map[string]struct{})
 	for _, h := range h.db.List() {
 		m[h] = struct{}{}
 	}
 	go h.t.Write(Message{"TorrentList", m, nil})
+	go h.shovel()
+	return h
+}
+
+func (h *ConnectionHandler) NewTorrent(t *database.Torrent) {
+	h.lock.RLock()
+	defer h.lock.RUnlock()
+	if _, seen := h.torrentList[t.Hash]; !seen {
+		h.t.Write(Message{"Torrents", nil, []*database.Torrent{t}})
+	}
+}
+
+// Shovels torrents into the db
+func (h *ConnectionHandler) shovel() {
 	for {
 		msg, err := h.t.Read()
 		if err != nil {
@@ -32,18 +44,11 @@ func (h *ConnectionHandler) shovel() {
 		}
 		switch msg.Code {
 		case "TorrentList":
-			torrents := make([]*database.Torrent, 0)
-			for _, hash := range h.db.List() {
-				if _, ok := msg.Torrents[hash]; !ok {
-					t, err := h.db.Get(hash)
-					if err != nil {
-						log.Printf("Torrent dissapeared? : %v", hash)
-						continue
-					}
-					torrents = append(torrents, t)
-				}
-			}
-			go h.t.Write(Message{"Torrents", nil, torrents})
+			// Once we know what the other side doesn't have, start dumping
+			h.lock.Lock()
+			h.torrentList = msg.Torrents
+			h.lock.Unlock()
+			go h.db.AddClient(h)
 		case "Torrents":
 			for _, t := range msg.Data {
 				log.Print("ADDDING TORRENT WITHOUT VERIFICATION!!!!")
