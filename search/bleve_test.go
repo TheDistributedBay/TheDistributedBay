bd035d9deb33a8b8933c6d14d4fa5af500291aae
Colin L. Rice <colin@daedrum.net>
2014-12-23 14:31:03 -0800

    Add 50 and 200 benchmark

--- search/bleve_test.go
+++ search/bleve_test.go
@@ -32,9 +32,31 @@ func BenchmarkBatchBleve10(b *testing.B) {
 	bleve.Flush()
 }
 
+func BenchmarkBatchBleve50(b *testing.B) {
+	bleve, _ := NewBleve("search.bleve")
+	bleve.BatchSize = 50
+	c := time.Now()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bleve.NewBatchedTorrent(testTorrent(fmt.Sprint(i), c))
+	}
+	bleve.Flush()
+}
+
 func BenchmarkBatchBleve100(b *testing.B) {
 	bleve, _ := NewBleve("search.bleve")
 	bleve.BatchSize = 100
+	c := time.Now()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		bleve.NewBatchedTorrent(testTorrent(fmt.Sprint(i), c))
+	}
+	bleve.Flush()
+}
+
+func BenchmarkBatchBleve200(b *testing.B) {
+	bleve, _ := NewBleve("search.bleve")
+	bleve.BatchSize = 200
 	c := time.Now()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
